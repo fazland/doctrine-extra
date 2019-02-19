0309fe48a685bc650d72508ecb76b6be3175f160
Massimiliano Braglia <massimiliano.braglia@gmail.com>
2019-02-19 11:30:42 +0100

    Add TimeSpan embeddable

---
+++ src/Entity/TimeSpan.php
@@ -0,0 +1,90 @@
+<?php declare(strict_types=1);
+
+namespace Fazland\DoctrineExtra\Entity;
+
+use Cake\Chronos\Chronos;
+use Doctrine\ODM\MongoDB\Mapping\Annotations as MongoDBODM;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Represents a time span with a start time and an end time.
+ * Start and end times are optional.
+ *
+ * @ORM\Embeddable()
+ * @MongoDBODM\EmbeddedDocument()
+ */
+class TimeSpan
+{
+    /**
+     * @var \DateTimeImmutable
+     *
+     * @ORM\Column(type="datetimetz_immutable", nullable=true)
+     */
+    private $start;
+
+    /**
+     * @var \DateTimeImmutable
+     *
+     * @ORM\Column(type="datetimetz_immutable", nullable=true)
+     */
+    private $end;
+
+    /**
+     * Gets the period start time, if set.
+     *
+     * @return \DateTimeImmutable
+     */
+    public function getStart(): ?\DateTimeImmutable
+    {
+        return $this->start;
+    }
+
+    /**
+     * Gets the period end time, if set.
+     *
+     * @return \DateTimeImmutable
+     */
+    public function getEnd(): ?\DateTimeImmutable
+    {
+        return $this->end;
+    }
+
+    /**
+     * Updates the time span.
+     *
+     * @param \DateTimeImmutable|null $start
+     * @param \DateTimeImmutable|null $end
+     */
+    public function update(?\DateTimeImmutable $start, ?\DateTimeImmutable $end): void
+    {
+        if (null !== $start && null !== $end && $start > $end) {
+            throw new \InvalidArgumentException('Start cannot be greater than end');
+        }
+
+        $this->start = $start;
+        $this->end = $end;
+    }
+
+    /**
+     * Checks whether the given date time is between the start and the end of
+     * this time span. Null limits are treated as Infinite.
+     *
+     * @param \DateTimeInterface $reference
+     *
+     * @return bool
+     */
+    public function contains(\DateTimeInterface $reference): bool
+    {
+        $reference = Chronos::instance($reference);
+
+        if (null !== $this->start && $reference->lt(Chronos::instance($this->start))) {
+            return false;
+        }
+
+        if (null !== $this->end && $reference->gte(Chronos::instance($this->end))) {
+            return false;
+        }
+
+        return true;
+    }
+}
